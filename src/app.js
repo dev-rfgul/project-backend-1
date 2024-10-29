381d57389356c64d1a6b7873e66b73b19c39f3e1
dev-rfgul <rfgul587@gmail.com>
2024-10-29 23:35:13 +0500

    completed video 12 and till here apparently there are no errors in the site but the user register api is not working , tried on postman web and vs code extension  as well

--- src/app.js
+++ src/app.js
@@ -14,4 +14,13 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//import routes 
+import userRouter from './routes/user.routes.js'
+app.use("/api/v1/users",userRouter)
+// the url from here will be like http:localhost:3000/api/v1/users/
+// but as it is handed over to the userRouter the urls will be handed over to the next route and the url will look like this http:localhost:3000/api/v1/user/register 
+// so instead of messing up the app.js we have implemented clean code and separation of concerns
+
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 //we need to load the env file first as it holds the urls keys and passwords.this is one apprroach of doing it and the other is to use the dotenv package but after importing we need to configure it as well
 // require("dotenv").config({path:'./env'});
-
+import express from 'express'
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+
+const app = express();
 
 dotenv.config({ path: "./env" });
 
