da290e906a5ebf913cd9ebae109b434039865877
dev-rfgul <rfgul587@gmail.com>
2024-10-30 13:28:24 +0500

    watched video no 13 till 30 min

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,43 @@
-import {asyncHandler} from '../utils/asyncHandler.js'
+import { asyncHandler } from '../utils/asyncHandler.js'
+import { ApiError } from "../utils/ApiError.js"
+import { User } from '../models/user.model.js'
 
 
 const registerUser = asyncHandler(async (req, res) => {
-    console.log("registerdUser called")
-    res.status(200).json({
-        message: "Ok"
+
+    //get user details from frontend 
+    // validation - not empty
+    // check if the user user is already registered -email / username
+    // check for images check for avatar
+    // upload it to cloudinary,avatar
+    // create user object create entry in DB
+    // remove password and refresh token field from response
+    //check for user creation
+    // retrun response
+    const { username, fullName, email, password } = req.body
+    console.log("email ", email)
+    console.log("password ", password)
+
+    // we can chceck all the fields by using alot of if else nothing wrong in this but we can use another approach starting from line 24
+    // if (fullName === "") {
+    //     throw new ApiError(400, "Full Name is required")
+    // }
+    if (
+        // in this line we are taking an array which will have all the values and using some function on it which will return true or false after that we are triming the fields and even then if any field is empty it will return true 
+        [fullName, email, password, username].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existed User = User.findOne({
+        // we can pass the value on which we want to find one which can be username or email but when we have to find by both like email and username shoudl  be unique in that case we can follow this approach
+        $or: [{ username }, { email }]
+        // and we can pass on as many objects as we want 
     })
+    if(existedUser){
+        throw new ApiError(409, "user with this email or username already exists")
+    }
+
 })
 
 export { registerUser } 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 import { Router } from 'express';
 import { registerUser } from '../controllers/user.controller.js'
-
+import {upload} from '../middlewares/multer.middlewares.js'
 const router = Router();
 
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    // we are using fields bcoz we need to handle multiple files if we had to upload only a single file we can use single instead of field
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
+    ]),
+    registerUser
+)
 export default router;
