3db9d7e9700d9cd8f4e4529a517e70a286794a9b
dev-rfgul <rfgul587@gmail.com>
2024-10-30 15:47:06 +0500

    video 13 completed and wrote user.controller.js / user registration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,13 @@
 import { asyncHandler } from '../utils/asyncHandler.js'
 import { ApiError } from "../utils/ApiError.js"
 import { User } from '../models/user.model.js'
+import { cloudinary, uploadOnCloudinary } from '../utils/cloudinary.js'
+import { ApiResponse } from '../utils/ApiResponse.js'
 
 
 const registerUser = asyncHandler(async (req, res) => {
 
+    // algorithm 
     //get user details from frontend 
     // validation - not empty
     // check if the user user is already registered -email / username
@@ -29,15 +32,51 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existed User = User.findOne({
+    const existedUser = User.findOne({
         // we can pass the value on which we want to find one which can be username or email but when we have to find by both like email and username shoudl  be unique in that case we can follow this approach
         $or: [{ username }, { email }]
         // and we can pass on as many objects as we want 
     })
-    if(existedUser){
+    if (existedUser) {
         throw new ApiError(409, "user with this email or username already exists")
     }
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.cover[0]?.path
 
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    const user = await User.create({
+        fullName,
+        email,
+        username,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        password,
+        username: username.toLowerCase()
+    })
+    // to verify wether the user has been created we can use find methord and find user by _id like this
+    // const createdUer = await User.findById(user._id) 
+
+    //in the following we had just verified wether the user has been created and we are removing the password and its refresh token this is a bit weird syntax of .select methord and in this we have to pass on the values we dont want to include with a -sign and by defualt everything is selected.
+    const createdUer = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    if (!createdUser) {
+        throw new ApiError(500, "something went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "user registered successfully")
+    )
 })
 
 export { registerUser } 
