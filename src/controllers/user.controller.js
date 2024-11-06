eeb23f6f3904ca77f5503786dded45d91d000d47
dev-rfgul <rfgul587@gmail.com>
2024-11-06 22:17:01 +0500

    watched video 20 till 20mins and wrote get watch history in this commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -390,12 +390,62 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
         throw new ApiError(404, "Channel not found ::user controller :: line no 390")
     }
     return res
-    .status(200)
-    .json(
-        200,
-        channel[0],
-        "user channel fetched successfully :: user controller :: line 398"
-    )
+        .status(200)
+        .json(
+            200,
+            channel[0],
+            "user channel fetched successfully :: user controller :: line 398"
+        )
+})
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            },
+
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1,
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, user[0].watchHistory, "Watch history fetched successfully :: user controller :: line no 446")
+        )
 })
 
 export {
@@ -409,4 +459,5 @@ export {
     updateAvatar,
     updateUserCoverImage,
     getUserChannelProfile,
+    getWatchHistory,
 }
